726caa73532884c57246ba415ce00de69650fe4b
Teodor Lagerqvist <teodor@pingpong.net>
2016-12-21 23:57:19 +0100

    show and hide project page

--- public/js/project-page.js
+++ public/js/project-page.js
@@ -1,18 +1,21 @@
 const ProjectPage = (function() {
 
+	const container = document.querySelector(".project-page");
 	const projectFiles = document.querySelector(".project-files");
 	const projectTitle = document.querySelector(".project-title");
 	const btnNewFile = document.querySelector(".button-new-file");
 	
 	btnNewFile.addEventListener("click", createNewFile);
 
-	XI.listen("navigate", payloads => {
+	XI.listen(["navigate","userLogin"], payloads => {
 		let uriParts = payloads[0];
 		if(uriParts[0]==='project') {
+			container.show();
 			const projectId = uriParts[1];
 			console.log("Load project", projectId);
 			openProject(projectId);
 		} else {
+			container.hide();
 			console.log("no project active");
 		}
 	}, true);
